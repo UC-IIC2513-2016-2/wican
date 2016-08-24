3191d6342fb9d73cd4bb42cf0c03fec7ad1f4224
Raúl Montes <raulmt@gmail.com>
2016-08-23 22:00:38 -0300

    Agrega validaciones del modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: {
+      with: /\A[a-zA-Z0-9_\.\+-]+@(?:[a-zA-Z0-9_-]+\.){1,}[a-zA-Z0-9_-]{2,5}\z/,
+      allow_blank: true
+    }
+  validates :password, presence: true, length: { minimum: 6, allow_blank: true }, confirmation: true
+  validates :first_name, presence: true, length: { minimum: 2, allow_blank: true }
+  validates :last_name, presence: true, length: { minimum: 2, allow_blank: true }
 end
