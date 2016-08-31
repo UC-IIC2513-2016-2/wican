feb3173813bf98a1ce734ffa344a63fcea0835a8
Raúl Montes <raulmt@gmail.com>
2016-08-31 00:01:01 -0300

    Controller y actions públicos de ONG generados

---
+++ app/controllers/ongs_controller.rb
@@ -0,0 +1,7 @@
+class OngsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- test/controllers/ongs_controller_test.rb
+++ test/controllers/ongs_controller_test.rb
@@ -1,48 +1,14 @@
 require 'test_helper'
 
 class OngsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @ong = ongs(:one)
-  end
-
   test "should get index" do
-    get ongs_url
+    get ongs_index_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_ong_url
+  test "should get show" do
+    get ongs_show_url
     assert_response :success
   end
 
-  test "should create ong" do
-    assert_difference('Ong.count') do
-      post ongs_url, params: { ong: { description: @ong.description, email: @ong.email, logo: @ong.logo, name: @ong.name, webpage: @ong.webpage } }
-    end
-
-    assert_redirected_to ong_url(Ong.last)
-  end
-
-  test "should show ong" do
-    get ong_url(@ong)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_ong_url(@ong)
-    assert_response :success
-  end
-
-  test "should update ong" do
-    patch ong_url(@ong), params: { ong: { description: @ong.description, email: @ong.email, logo: @ong.logo, name: @ong.name, webpage: @ong.webpage } }
-    assert_redirected_to ong_url(@ong)
-  end
-
-  test "should destroy ong" do
-    assert_difference('Ong.count', -1) do
-      delete ong_url(@ong)
-    end
-
-    assert_redirected_to ongs_url
-  end
 end
