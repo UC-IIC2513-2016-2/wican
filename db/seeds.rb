17889f851a0352748a8844d78b9651e174cc29fb
Raúl Montes <raulmt@gmail.com>
2016-08-30 21:26:29 -0300

    Seeds con datos iniciales para la BD usando Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+10.times do
+  ong = Ong.create(
+    name: Faker::Company.name,
+    email: Faker::Internet.email,
+    logo: Faker::Company.logo,
+    description: Faker::Lorem.sentences(2).join(' '),
+    webpage: Faker::Internet.url
+  )
+  rand(5..10).times do
+    ong.initiatives << Initiative.new(
+      title: Faker::Hipster.sentence,
+      description: Faker::Hipster.paragraph,
+      image: Faker::Placeholdit.image('350x350'),
+      active: Faker::Boolean.boolean,
+      hashtag: "##{Faker::Hipster.word}"
+    )
+  end
+end
