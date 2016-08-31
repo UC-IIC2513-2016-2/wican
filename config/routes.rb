71505a9cf9c589399a025024612db48813de45cf
Raúl Montes <raulmt@gmail.com>
2016-08-31 00:01:28 -0300

    Rutas para vistas públicas de ONGs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :ongs
   end
+  resources :ongs, only: [:index, :show]
   resources :initiatives
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
