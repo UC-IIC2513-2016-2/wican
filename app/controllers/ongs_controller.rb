2c8fbb9411d88773d6cccd12d7e001974614db7e
Raúl Montes <raulmt@gmail.com>
2016-09-04 22:13:29 -0300

    Contenido de vistas públicas para ONGs

--- app/controllers/ongs_controller.rb
+++ app/controllers/ongs_controller.rb
@@ -1,7 +1,9 @@
 class OngsController < ApplicationController
   def index
+    @ongs = Ong.all
   end
 
   def show
+    @ong = Ong.find(params[:id])
   end
 end
--- app/models/ong.rb
+++ app/models/ong.rb
@@ -1,3 +1,7 @@
 class Ong < ApplicationRecord
   has_many :initiatives
+
+  def some_initiatives(max = 5)
+    initiatives.where(active: true).limit(max).order('RANDOM()')
+  end
 end
